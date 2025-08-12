7b013cf4e261269ff9e24b7aac76fae8dabfbb71
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2025-08-11 21:06:51 -0500

    test: add Redis and application context tests

---
+++ src/test/java/com/labs/repartitioner/RepartitionerApplicationTest.java
@@ -0,0 +1,11 @@
+package com.labs.repartitioner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepartitionerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
