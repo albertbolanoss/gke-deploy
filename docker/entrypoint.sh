0291af8958240012a5377412441ffacdf48d51ff
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2025-09-04 09:07:26 -0500

    create entrypoint multi environment loading

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,33 +1,48 @@
+
 #!/bin/bash
 
 set -e
 date
 
-SECRET_ENV_PATH="/etc/secrets/secrets.env"
-
+SECRETS_DIR="/etc/secrets"
+TEMP_DIR="/tmp/cleaned_secrets"
 max_wait=30
 waited=0
 
 echo "Starting Entrypoint."
 
-while [ ! -f "$SECRET_ENV_PATH" ] && [ $waited -lt $max_wait ]; do
+# wait until almost one file exist
+while [ -z "$(ls -A "$SECRETS_DIR" 2>/dev/null)" ] && [ $waited -lt $max_wait ]; do
   sleep 1
   waited=$((waited + 1))
-  echo "waiting for mount the volume."
+  echo "Waiting for secrets to be mounted..."
 done
-
-if [ -f "$SECRET_ENV_PATH" ]; then
-  echo "Sanitizing $SECRET_ENV_PATH"
-  tr -d '\r' < "$SECRET_ENV_PATH" > "/tmp/cleaned_secrets.env"
-  SECRET_ENV_PATH="/tmp/cleaned_secrets.env"
-
-  echo "Loading environment variables from $SECRET_ENV_PATH"
-  set -a
-  . "$SECRET_ENV_PATH"
-  set +a
-else
-  echo "ERROR: $SECRET_ENV_PATH not found after $max_wait seconds."
+if [ -z "$(ls -A "$SECRETS_DIR" 2>/dev/null)" ]; then
+  echo "ERROR: No secrets found in $SECRETS_DIR after $max_wait seconds."
+  exit 1
 fi
+
+# Create the tmp/cleaned_secrets folder
+mkdir -p "$TEMP_DIR"
+
+# Sanitizing an load the environment variables
+load_env_from_file() {
+  local secret_file="$1"
+  local cleaned_file="$TEMP_DIR/$(basename "$secret_file")"
+
+  echo "Sanitizing $secret_file"
+  tr -d '\r' < "$secret_file" > "$cleaned_file"
+
+  echo "Loading environment variables from $cleaned_file"
+  set -a
+  . "$cleaned_file"
+  set +a
+}
+
+# Process each found file in /etc/secrets
+for secret_file in "$SECRETS_DIR"/*; do
+  [ -f "$secret_file" ] && load_env_from_file "$secret_file"
+done
 
 echo "Starting Java application"
 exec java -jar "/app/${JAR_NAME}.jar" "$@"
